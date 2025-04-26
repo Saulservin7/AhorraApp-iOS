773b7160aa4a52589f58ededcc28746c4d179820
Servin <saulservin7@gmail.com>
2025-04-25 19:19:54 -0600

    Initial Commit

---
+++ AhorrApp/AhorrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AhorrAppApp.swift
+//  AhorrApp
+//
+//  Created by Saúl  Servín  on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct AhorrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AhorrAppTests/AhorrAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AhorrAppTests.swift
+//  AhorrAppTests
+//
+//  Created by Saúl  Servín  on 4/25/25.
+//
+
+import Testing
+@testable import AhorrApp
+
+struct AhorrAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
